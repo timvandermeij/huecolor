652d54c7af66bbe44c542f3977183525ee153916
Tim van der Meij <timvandermeij@gmail.com>
2014-11-02 16:09:50 +0100

    Miscellaneuous fixes for MainActivity
    - Code style fixes
    - Comment fixes
    - Smaller equivalent code fixes

--- app/src/main/java/nl/liacs/huecolor/MainActivity.java
+++ app/src/main/java/nl/liacs/huecolor/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,20 +30,19 @@ public class MainActivity extends Activity {
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // create Intent to take a picture and return control to the calling application
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Create Intent to take a picture and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                try {
-                    fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "Could not create an image file for photo",
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+            try {
+                fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // Create a file to save the image
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Could not create an image file for photo", Toast.LENGTH_LONG).show();
+                return;
+            }
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // Set the image file name
 
-                // start the image capture Intent
-                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            // Start the image capture intent
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
             }
         });
 
@@ -53,14 +50,13 @@ public class MainActivity extends Activity {
         browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File mPath = new File(Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES).toString());
+                File mPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString());
                 FileDialog fileDialog = new FileDialog(MainActivity.this, mPath);
                 fileDialog.setFileEndsWith(".jpg");
                 fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
                     public void fileSelected(File file) {
-                        Log.d("HueColor", "selected file " + file.toString());
-                        fileUri = Uri.fromFile(file);
+                    Log.d("HueColor", "selected file " + file.toString());
+                    fileUri = Uri.fromFile(file);
                     }
                 });
                 fileDialog.showDialog();
@@ -68,55 +64,50 @@ public class MainActivity extends Activity {
         });
     }
 
-
-    /** Create a file Uri for saving an image or video */
+    /*
+     * Create a file URI for saving an image or video
+     */
     private static Uri getOutputMediaFileUri(int type){
         return Uri.fromFile(getOutputMediaFile(type));
     }
 
-    /** Create a File for saving an image or video */
+    /*
+     * Create a file for saving an image or video
+     */
     private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
+        // TODO: check that the SDCard is mounted using Environment.getExternalStorageState()
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "HueColor");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
+                                        Environment.DIRECTORY_PICTURES), "HueColor");
 
         // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                Log.d("HueColor", "failed to create directory");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("HueColor", "Failed to create directory");
                 return null;
             }
         }
 
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else {
-            return null;
+        if (type == MEDIA_TYPE_IMAGE) {
+            return new File(mediaStorageDir.getPath() + File.separator +
+                            "IMG_"+ timeStamp + ".jpg");
         }
-
-        return mediaFile;
+        return null;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Handle the saved camera photo file
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            // Handle the saved camera photo file
             if (resultCode == RESULT_OK) {
-                // Image captured and saved to fileUri specified in the Intent
+                // Image captured and saved to file URI specified in the intent
                 Toast.makeText(this, "Image saved to:\n" + fileUri, Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
-                // Image capture failed, advise user
+                // Image capture failed
                 Toast.makeText(this, "Image capture failed", Toast.LENGTH_LONG).show();
             }
         }
