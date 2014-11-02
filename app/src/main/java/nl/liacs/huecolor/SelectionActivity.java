384969dfcc6c7c4e3e805e1b0f82db57749fd045
Tim van der Meij <timvandermeij@gmail.com>
2014-11-02 17:13:44 +0100

    Load example drawable for object selection

--- app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
+++ app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
@@ -1,12 +1,18 @@
 package nl.liacs.huecolor;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SelectionActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Drawable drawable = getResources().getDrawable(R.drawable.example);
+        image.setImageDrawable(drawable);
     }
 }
