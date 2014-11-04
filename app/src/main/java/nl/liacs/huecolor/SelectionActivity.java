d096baf69856ccd405a05f51def4d4bdee207e97
Tim van der Meij <timvandermeij@gmail.com>
2014-11-04 23:38:49 +0100

    Improved selection code
    - Removed layout file that is not used anymore
    - Added comments to new selection activity
    - Reimplemented grayscale conversion

--- app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
+++ app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
@@ -6,73 +6,94 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+/**
+ * Based on https://code.google.com/p/apidemos/source/browse/trunk/ApiDemos/src/com/example/android/apis/graphics/FingerPaint.java
+ */
 public class SelectionActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new SelectionView(this));
 
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setDither(true);
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(12);
+        // Use a custom view for the image.
+        setContentView(new SelectionView(this));
     }
 
-    private Paint       paint;
-
+    /**
+     * Custom view for showing a grayscale image and drawing a path on the image.
+     */
     public class SelectionView extends View {
+        private Paint paint;
         private Bitmap bitmap;
         private Canvas canvas;
         private Path path;
         private Paint bitmapPaint;
+        private float startX, startY; // Used for calculating new point of line
+        private static final float TOUCH_TOLERANCE = 4; // Defines how quickly we should draw a line
 
-        public SelectionView(Context c) {
-            super(c);
+        public SelectionView(Context context) {
+            super(context);
 
-            BitmapFactory.Options myOptions = new BitmapFactory.Options();
-            myOptions.inDither = true;
-            myOptions.inScaled = false;
-            myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            myOptions.inPurgeable = true;
+            // Set the image options.
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inDither = true;
+            options.inScaled = false;
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            options.inPurgeable = true;
 
-            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.example, myOptions);
+            // Load the image and define the canvas on top of it.
+            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.example, options);
             canvas = new Canvas(bitmap.copy(Bitmap.Config.ARGB_8888, true));
             path = new Path();
             bitmapPaint = new Paint(Paint.DITHER_FLAG);
+
+            // Define the paint for the selection line.
+            paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            paint.setStrokeWidth(10);
+
+            // Convert the image to grayscale.
+            ColorMatrix matrix = new ColorMatrix();
+            matrix.setSaturation(0);
+            ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+            bitmapPaint.setColorFilter(filter);
         }
 
         @Override
-        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-            super.onSizeChanged(w, h, oldw, oldh);
+        protected void onSizeChanged(int w, int h, int oldWidth, int oldHeight) {
+            super.onSizeChanged(w, h, oldWidth, oldHeight);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            //canvas.drawColor(0x0);
+            // Update the previous path and draw the new path.
             canvas.drawBitmap(bitmap, 0, 0, bitmapPaint);
             canvas.drawPath(path, paint);
         }
 
-        private float startX, startY;
-        private static final float TOUCH_TOLERANCE = 4;
-
-        private void touch_start(float x, float y) {
+        private void touchStart(float x, float y) {
+            // Start a new path when the user taps on the screen.
             path.reset();
             path.moveTo(x, y);
             startX = x;
             startY = y;
         }
-        private void touch_move(float x, float y) {
+
+        private void touchMove(float x, float y) {
+            // Add a point to the path when the user moves his or her finger.
+            // Sensitivity is added for the sake of performance.
             float dx = Math.abs(x - startX);
             float dy = Math.abs(y - startY);
             if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
@@ -81,31 +102,35 @@ public class SelectionActivity extends Activity {
                 startY = y;
             }
         }
-        private void touch_up() {
+
+        private void touchUp() {
             path.lineTo(startX, startY);
-            // commit the path to our offscreen
+            // Commit the path
             canvas.drawPath(path, paint);
-            // kill this so we don't double draw
+            // Avoid double drawing
             path.reset();
         }
 
         @Override
         public boolean onTouchEvent(MotionEvent event) {
+            // Handle touch events: tap, move and finger up.
             float x = event.getX();
             float y = event.getY();
 
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    touch_start(x, y);
+                    touchStart(x, y);
                     invalidate();
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    touch_move(x, y);
+                    touchMove(x, y);
                     invalidate();
                     break;
                 case MotionEvent.ACTION_UP:
-                    touch_up();
+                    touchUp();
                     invalidate();
+                    break;
+                default:
                     break;
             }
             return true;
