04630d606dabe2fbf5c61221ada115bc6e7708bd
Justist <skl_oh2@hotmail.com>
2014-11-18 23:34:05 +0100

    Actionbar in SelectionView customized
    Now there is a button! Which you can click!
    And which in the future features filters!

--- app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
+++ app/src/main/java/nl/liacs/huecolor/SelectionActivity.java
@@ -1,9 +1,13 @@
 package nl.liacs.huecolor;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Based on https://code.google.com/p/apidemos/source/browse/trunk/ApiDemos/src/com/example/android/apis/graphics/FingerPaint.java
@@ -23,5 +27,20 @@ public class SelectionActivity extends Activity {
         // Use a custom view for the image.
         SelectionView initialView = new SelectionView(this, fileUri);
         setContentView(initialView);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        View view = View.inflate(getApplicationContext(), R.layout.selection_bar, null);
+        actionBar.setCustomView(view);
+
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.filterButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "It would be nice if a filter was applied now.",
+                               Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
